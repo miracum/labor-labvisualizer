d214c54013036a19ab3d7d006bddf66a8a6caff8
Lorenz Kapsner <lorenz.kapsner@uk-erlangen.de>
2020-04-14 09:17:05 +0200

    fixed bug in set_env_vars when '=' in value

--- data-raw/debugging.R
+++ data-raw/debugging.R
@@ -2,16 +2,7 @@
 lib_path <- "/home/user/development/Rpackages/labvisualizer/ume_labvisualizer_deployment/libs/"
 utils_path <- "/home/user/development/Rpackages/labvisualizer/ume_labvisualizer_deployment/utilities/"
 
-envs <- readLines(paste0(lib_path, "../deploy/.env"))
-
-args <- sapply(envs, FUN = function(x) {
-  l <- unlist(strsplit(x, "="))
-  env <- paste(l[2:length(l)], collapse = "")
-  names(env) <- unlist(strsplit(x, "="))[[1]]
-  return(env)
-}, USE.NAMES = F)
-
-do.call(Sys.setenv, as.list(args))
+DIZutils::set_env_vars(paste0(lib_path, "../deploy/.env"))
 
 launch_app(
   db_name = "ORACLE",
--- inst/application/server.R
+++ inst/application/server.R
@@ -2,8 +2,8 @@ shiny::shinyServer(function(input, output, session) {
     # define reactive values here
     rv <- shiny::reactiveValues(
         db_name = db_name,
-        utils_path = utils_path,
-        lib_path = lib_path
+        utils_path = DIZutils::clean_path_name(utils_path),
+        lib_path = DIZutils::clean_path_name(lib_path)
     )
 
     input_reactive <- reactive({
